507a4596a2652eba647fb666b982100890957045
nannany <ymym1990ymym@gmail.com>
2022-03-19 14:14:36 +0900

    番号フル

--- examples/simple-table-create.rs
+++ examples/simple-table-create.rs
@@ -5,22 +5,29 @@ use didactic_funicular::table::SimpleTable;
 
 
 fn main() -> Result<()> {
+    // 1
     let disk = DiskManager::open("simple.rly")?;
     let pool = BufferPool::new(10);
     let mut bufmgr = BufferPoolManager::new(disk, pool);
 
+    // 2
     let mut table = SimpleTable {
         meta_page_id: PageId(0),
         num_key_elems: 1,
     };
+
+    // 3
     table.create(&mut bufmgr)?;
     dbg!(&table);
+
+    // 4
     table.insert(&mut bufmgr, &[b"z", b"Alice", b"Smith"])?;
     table.insert(&mut bufmgr, &[b"x", b"Bob", b"Johnson"])?;
     table.insert(&mut bufmgr, &[b"y", b"Charlie", b"Williams"])?;
     table.insert(&mut bufmgr, &[b"w", b"Dave", b"Miller"])?;
     table.insert(&mut bufmgr, &[b"v", b"Eve", b"Brown"])?;
 
+    // 5
     bufmgr.flush()?;
     Ok(())
 }
