eec94bae27e9efc9ab5c0ef5f716eae23e2e35e0
nannany <ymym1990ymym@gmail.com>
2022-03-19 20:22:55 +0900

    table-index.rs動かす

---
+++ examples/table-index.rs
@@ -0,0 +1,27 @@
+use anyhow::Result;
+
+use didactic_funicular::buffer::{BufferPool, BufferPoolManager};
+use didactic_funicular::disk::{DiskManager, PageId};
+use didactic_funicular::query::{IndexScan, PlanNode, TupleSearchMode};
+use didactic_funicular::tuple;
+
+// SELECT * WHERE last_name = 'Smith'
+// with index
+fn main() -> Result<()> {
+    let disk = DiskManager::open("table.rly")?;
+    let pool = BufferPool::new(10);
+    let mut bufmgr = BufferPoolManager::new(disk, pool);
+
+    let plan = IndexScan {
+        table_meta_page_id: PageId(0),
+        index_meta_page_id: PageId(2),
+        search_mode: TupleSearchMode::Key(&[b"Smith"]),
+        while_cond: &|skey| skey[0].as_slice() == b"Smith",
+    };
+    let mut exec = plan.start(&mut bufmgr)?;
+
+    while let Some(record) = exec.next(&mut bufmgr)? {
+        println!("{:?}", tuple::Pretty(&record));
+    }
+    Ok(())
+}
