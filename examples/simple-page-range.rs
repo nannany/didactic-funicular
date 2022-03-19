a940655942b78da560304e4b42736f5a47290c01
nannany <ymym1990ymym@gmail.com>
2022-03-19 14:55:39 +0900

    simple-page-range.rsを動かせるように

---
+++ examples/simple-page-range.rs
@@ -0,0 +1,25 @@
+use anyhow::Result;
+
+use didactic_funicular::btree::{BTree, SearchMode};
+use didactic_funicular::buffer::{BufferPool, BufferPoolManager};
+use didactic_funicular::disk::{DiskManager, PageId};
+use didactic_funicular::tuple;
+
+fn main() -> Result<()> {
+    let disk = DiskManager::open("simple.rly")?;
+    let pool = BufferPool::new(10);
+    let mut bufmgr = BufferPoolManager::new(disk, pool);
+
+    let btree = BTree::new(PageId(0));
+    let mut search_key = vec![];
+    tuple::encode([b"y"].iter(), &mut search_key);
+    let mut iter = btree.search(&mut bufmgr, SearchMode::Key(search_key))?;
+
+    while let Some((key, value)) = iter.next(&mut bufmgr)? {
+        let mut record = vec![];
+        tuple::decode(&key, &mut record);
+        tuple::decode(&value, &mut record);
+        println!("{:?}", tuple::Pretty(&record));
+    }
+    Ok(())
+}
