1ffd5899936265ac2b4b0186f6bdf4772c47add8
minami.yoshihiko <nannany@hey.jp>
2022-03-20 16:00:53 +0900

    parsesqlつかてみる

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
+use sqlparser::dialect::GenericDialect;
+use sqlparser::parser::Parser;
+
 mod disk;
 mod buffer;
 
 fn main() {
-    println!("Hello, world!");
+    let dialect = GenericDialect {};
+
+    let sql = "SELECT a, b, 123, myfunc(b) \
+    FROM table_1 \
+    WHERE a > b AND b < 100 \
+    ORDER BY a DESC, b";
+
+    let ast = Parser::parse_sql(&dialect, sql).unwrap();
+    println!("AST: {:?}", ast);
 }
